95e6a2cad5ad245dbc30655251b17626f6f41b77
Federico Berto <berto.federico2@gmail.com>
2020-11-09 22:10:20 +0900

    First LSTM working model added

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import datetime
 from datetime import timedelta
 import numpy as np
+import torch
+from numpy import array
+
 
 # Transform all the elements in the dataset into date objects and sort them
 def convert_to_dates(dates):
@@ -15,3 +18,28 @@ def time_diff_sec(t_f, t_0):
 
 def to_array(data):
     return np.array(data).reshape(-1, 1)
+
+# def create_inout_sequences(dt, tw):
+#     inout_seq = []
+#     L = dt.shape[1]
+#     for i in range(L-tw):
+#         train_seq = torch.zeros(NUM_FEATURES, tw)
+#         for j in range(NUM_FEATURES):
+#             train_seq[j]= dt[j][i:i+tw]
+#         train_label = dt[0][i+tw:i+tw+1]
+#         inout_seq.append((train_seq ,train_label))
+#     print(inout_seq)
+#     return inout_seq
+
+def create_inout_sequences(dt, tw, n_features=5):
+    '''We create a list of training data divided in inputs X and outputs y'''
+    X = []
+    y = []
+    L = dt.shape[1]
+    for i in range(L-tw):
+        train_seq = torch.zeros(n_features, tw)
+        for j in range(n_features):
+            train_seq[j]= dt[j][i:i+tw]
+        train_label = dt[0][i+tw:i+tw+1] 
+        X.append(train_seq.numpy()); y.append(train_label.numpy())
+    return array(X), array(y)
